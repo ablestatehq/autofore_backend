ee4916605e2a8e5c493e2724e8a5f53eda86e604
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-09-07 14:13:30 +0300

    feat: verify otp

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const sendOtp = require("./src/handlers/sendOtp");
 const isUserRegistered = require("./src/handlers/isUserRegistered");
-const createUser = require("./src/handlers/createUser")
+const createUser = require("./src/handlers/createUser");
+const verifyOtp = require("./src/handlers/verifyOtp");
 const app = express();
 const port = 5000;
 
@@ -12,7 +13,10 @@ app.use(express.json());
 app.post("/messages/send-otp", sendOtp);
 app.post("/users/is-registered", isUserRegistered);
 app.post("/users/create", createUser);
+app.post("/verify-otp", verifyOtp);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port  http://localhost:${port}`);
+  console.log(
+    `Autofore's ready and live on port  https:www.autofore.co:${port}`
+  );
 });
