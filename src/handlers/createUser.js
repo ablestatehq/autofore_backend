37661511c3eca94876616ffa22f56b94cd609300
Ablestate <88195785+ablestatehq@users.noreply.github.com>
2023-09-21 19:20:37 +0300

    Update createUser.js

--- src/handlers/createUser.js
+++ src/handlers/createUser.js
@@ -25,7 +25,7 @@ const createUser = async (req, res) => {
         const customerMembership = await teams.createMembership(
           team,
           [],
-          "https://aw2.ablestate.cloud/",
+          "https://aw2.ablestate.cloud",
           undefined,
           user.$id
         );
